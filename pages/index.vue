32ed12a232c19bc6cccfab2ff493ba3e53ad05ed
abdulbasit <mrspeed518@gmail.com>
2020-11-04 22:40:04 +0100

    removed eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <div class="ml"></div>
+    <div class="ml">
+      hello
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {}
 </script>
-
 <style lang="scss" scoped>
-
 </style>
